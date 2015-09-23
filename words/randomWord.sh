51049be4635aabbd100f16e993bb203e7856d13b
Hellfar <pierre.louchet@gmail.com>
2015-09-23 16:16:11 +0200

    Giving list file by parameter in Words.

--- words/randomWord.sh
+++ words/randomWord.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-let t=`let MAX=\`cat wordlist_en.txt | wc -l\`;expr \`cat /dev/urandom | tr -dc 0-9 | fold -w${#MAX} | head -1 | sed 's/^0*//;'\` % $MAX + 1`;cat wordlist_en.txt | sed -n "$t,$t p"
+if [ -z "$1" ]
+then
+	export list="wordlist_en.txt"
+else
+	export list="$1"
+fi
+
+let t=`let MAX=\`cat $list | wc -l\`;expr \`cat /dev/urandom | tr -dc 0-9 | fold -w${#MAX} | head -1 | sed 's/^0*//;'\` % $MAX + 1`;cat wordlist_en.txt | sed -n "$t,$t p"
